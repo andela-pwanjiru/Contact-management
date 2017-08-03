27aab50bb41e59d97433417024a86ee3e3c83799
Penina Wanjiru <penina.wanjiru@gwfathom.com>
2017-08-03 13:46:49 +0300

    Create models for person and contacts

--- v1/models.py
+++ v1/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Contacts(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=30)
+    fax = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    contacts = models.ForeignKey(Contacts, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
